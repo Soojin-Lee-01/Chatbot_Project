f3b080f7e9c7d8e015f07c5d680eda8e29dcc7fb
Soojin-Lee-01 <sojinlee1004@naver.com>
2022-10-05 23:59:11 +0900

    facility_info update

--- Facility_information/Facility_information.py
+++ Facility_information/Facility_information.py
@@ -98,7 +98,7 @@ def cb_button(update, context):
             bot.send_message(telegram_id, '연중무휴 24시간')
 
 
-
+## 일단 임의로 생성
 def operating_time():
     time.sleep(5)
     print('.')
@@ -110,15 +110,16 @@ def number_books():
 
 def loan_return():
     time.sleep(5)
-    print('대출 반납')
+    print('.')
 
 
 def overdue_penalty():
     time.sleep(3)
-    print('연체 안내 처리 완료')
+    print('.')
 
 
 
+#시나리오 수정 예정
 def handler1(update, context):
     user_text = update.message.text
     if user_text == "반납":
